efa02d19b40a92ae34da5c785c9877f09d437d9b
KiwiLemons <26863753+KiwiLemons@users.noreply.github.com>
2024-01-31 02:29:15 -0600

    utilize local development server + add User model and controller

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AdventureLoggerBackend.Controllers;
+using AdventureLoggerBackend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AdventureLoggerBackendContext>(options =>
+    options.UseMySQL(builder.Configuration.GetConnectionString("AdventureLoggerBackendContext") ?? throw new InvalidOperationException("Connection string 'AdventureLoggerBackendContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -7,9 +13,13 @@ builder.Services.AddControllersWithViews();
 if (builder.Environment.IsProduction())
 {
     var port = Environment.GetEnvironmentVariable("PORT");
-    if (String.IsNullOrEmpty(port))
+    if (!String.IsNullOrEmpty(port))
         builder.WebHost.UseUrls($"http://*:{port}");
 }
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
