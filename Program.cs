3da489c14297598eeee88204d52f5f90ee20e14e
Kaiden <ghostdog12321@gmail.com>
2024-02-02 03:14:49 -0600

    Created log in page for users

--- Program.cs
+++ Program.cs
@@ -5,11 +5,14 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Use the new minimal hosting model
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
